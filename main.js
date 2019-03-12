60ee7d71b176ee4aa3807851fda2e79e6d304eae
teamtunfiske <jbaa14@student.aau.dk>
2019-03-12 13:20:31 +0100

    more main work

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 let places = {
-    town:{name:"town",pos:[0,0]},
+    town:{name:"town",type:"town",dificulity:1,pos:[0,0]},
     forrest:{name:"forrest",pos:[10,0]},
     mine:{name:"mine",pos:[0,10]},
 };
 
 class Player{
-constructor(){
+constructor(div){
+    this.div = div;
     this.name = "john doe";
-    this.inv = new Inventory(20,document.getElementById("inventory"));
+    this.inv = new Inventory(8  ,document.getElementById("inventory"));
     this.level = 1;
     this.exp = 0;
+    this.questsCompleted = 0;
+    this.upgrades = {
+        armor: 1,
+        weapon: 1,
+        inventory:1
+    };
     this.equipment = {
         head: new Item("none","",0,1,"head",{}),
         body: new Item("none","",0,1,"body",{}),
@@ -18,7 +25,31 @@ constructor(){
     };
     this.place = places.town;
 
+    this.addExp = function (exp) {
+        this.exp += exp;
+        if(this.exp >= this.ExpToLevel())
+            this.level++;
+        this.DrawPlayer()
+    };
+    /**
+     * @return {number}
+     */
+    this.ExpToLevel = function(){
+        return (Math.pow(2,this.level))+(this.level*this.level*100)-2
+    };
+
+    this.DrawPlayer = function () {
+
+    }
 }
 }
 
 let player = new Player();
+
+/**
+ * @return {number}
+ */
+function Distance(point1,point2) {
+    return Math.sqrt( Math.pow((point1[0]-point2[0]), 2) + Math.pow((point1[1]-point2[1]), 2) );
+
+}
